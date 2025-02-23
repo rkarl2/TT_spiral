de5d23e3e9fecee74a8500566046b2d83d9ae5e1
Ryan <ryan.c.karl@gmail.com>
2025-02-23 14:42:33 -0600

    initial Commit

---
+++ src/cordic.v
@@ -0,0 +1,108 @@
+
+`default_nettype	none
+//
+module	topolar(i_clk, i_reset, i_ce, i_xval, i_yval,
+		o_phase);
+	localparam	IW=7,	// The number of bits in our inputs
+			OW=4,// The number of output bits to produce
+			NSTAGES=3,
+			XTRA= 0,// Extra bits for internal precision
+			WW=7,	// Our working bit-width
+			PW=4;	// Bits in our phase variables
+	input					i_clk, i_reset, i_ce;
+	input	wire	signed	[(IW-1):0]	i_xval, i_yval;
+	output	reg		[(OW-1):0]	o_phase;
+
+	wire	signed [(WW-1):0]	e_xval, e_yval;
+	assign	e_xval = { {(2){i_xval[(IW-1)]}}, i_xval, {(WW-IW-2){1'b0}} };
+	assign	e_yval = { {(2){i_yval[(IW-1)]}}, i_yval, {(WW-IW-2){1'b0}} };
+
+	// Declare variables for all of the separate stages
+	reg	signed	[(WW-1):0]	xv	[0:NSTAGES];
+	reg	signed	[(WW-1):0]	yv	[0:NSTAGES];
+	reg		[(PW-1):0]	ph	[0:NSTAGES];
+
+
+
+	// First stage, map to within +/- 45 degrees
+	always @(posedge i_clk)
+	if (i_reset)
+	begin
+		xv[0] <= 0;
+		yv[0] <= 0;
+		ph[0] <= 0;
+	end else if (i_ce)
+		case({i_xval[IW-1], i_yval[IW-1]})
+		2'b01: begin // Rotate by -315 degrees
+			xv[0] <=  e_xval - e_yval;
+			yv[0] <=  e_xval + e_yval;
+			ph[0] <= 19'h70000 >> (19-PW-1);
+			end
+		2'b10: begin // Rotate by -135 degrees
+			xv[0] <= -e_xval + e_yval;
+			yv[0] <= -e_xval - e_yval;
+			ph[0] <= 19'h30000 >> (19-PW-1);
+			end
+		2'b11: begin // Rotate by -225 degrees
+			xv[0] <= -e_xval - e_yval;
+			yv[0] <=  e_xval - e_yval;
+			ph[0] <= 19'h50000>> (19-PW-1);
+			end
+		default: begin // Rotate by -45 degrees
+			xv[0] <=  e_xval + e_yval;
+			yv[0] <= -e_xval + e_yval;
+			ph[0] <= 19'h10000>> (19-PW-1);
+			end
+		endcase
+	wire	[18:0]	cordic_angle [0:(NSTAGES-1)];
+	assign	cordic_angle[ 0] = 19'h0_9720>> (19-PW-1); //  26.565051 deg
+	assign	cordic_angle[ 1] = 19'h0_4fd9>> (19-PW-1); //  14.036243 deg
+	assign	cordic_angle[ 2] = 19'h0_2888>> (19-PW-1); //   7.125016 deg
+
+	genvar	i;
+	generate for(i=0; i<NSTAGES; i=i+1) begin : TOPOLARloop
+		always @(posedge i_clk)
+		// Here's where we are going to put the actual CORDIC
+		// rectangular to polar loop.  Everything up to this
+		// point has simply been necessary preliminaries.
+		if (i_reset)
+		begin
+			xv[i+1] <= 0;
+			yv[i+1] <= 0;
+			ph[i+1] <= 0;
+		end else if (i_ce)
+		begin
+			if ((cordic_angle[i] == 0)||(i >= WW))
+			begin // Do nothing but move our vector
+			// forward one stage, since we have more
+			// stages than valid data
+				xv[i+1] <= xv[i];
+				yv[i+1] <= yv[i];
+				ph[i+1] <= ph[i];
+			end else if (yv[i][(WW-1)]) // Below the axis
+			begin
+				// If the vector is below the x-axis, rotate by
+				// the CORDIC angle in a positive direction.
+				xv[i+1] <= xv[i] - (yv[i]>>>(i+1));
+				yv[i+1] <= yv[i] + (xv[i]>>>(i+1));
+				ph[i+1] <= ph[i] - cordic_angle[i];
+			end else begin
+				// On the other hand, if the vector is above the
+				// x-axis, then rotate in the other direction
+				xv[i+1] <= xv[i] + (yv[i]>>>(i+1));
+				yv[i+1] <= yv[i] - (xv[i]>>>(i+1));
+				ph[i+1] <= ph[i] + cordic_angle[i];
+			end
+		end
+	end endgenerate
+
+always @(posedge i_clk)
+	if (i_reset)
+	begin
+		o_phase <= 0;
+	end else if (i_ce)
+	begin
+		o_phase <= ph[NSTAGES][(PW-1):(PW-OW)];
+	end
+
+endmodule
---
+++ src/spirals.v
@@ -0,0 +1,72 @@
+module tt_um_rkarl_Spiral(
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] uio_in,   // IOs: Input path
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+  input  wire       clk,      // clock
+  input  wire       rst_n     // reset_n - low to reset
+);
+
+  wire hsync;
+  wire vsync;
+  wire [1:0] R;
+  wire [1:0] G;
+  wire [1:0] B;
+  wire video_active;
+  wire [9:0] pix_x;
+  wire [9:0] pix_y;
+  
+  wire [1:0] speed = ~ui_in[1:0];
+  wire [2:0] background = ui_in[4:2];
+  wire [2:0] foreGround = ui_in[7:5];
+  hvsync_generator hvsync_gen(
+    .clk(clk),
+    .reset(~rst_n),
+    .hsync(hsync),
+    .vsync(vsync),
+    .display_on(video_active),
+    .hpos(pix_x),
+    .vpos(pix_y)
+  );
+  
+  wire signed [9:0] normilizedX = (pix_x -323);
+  wire signed [9:0] normilizedY = (pix_y -243);
+  wire [3:0] angle;
+  reg [4:0] angleOffset;
+  wire [3:0] adjustedAngle = angle + angleOffset[4:1];
+
+  always @(posedge vsync)
+	begin
+		angleOffset <= angleOffset+speed;
+	end 
+  
+  topolar cordicAlg(
+  .i_clk(clk),
+  .i_reset(~rst_n),
+  .i_ce(1'b1),
+  .i_xval(normilizedX[9:3]),
+  .i_yval(normilizedY[9:3]),
+  .o_phase(angle)
+  );
+  
+  assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+
+  wire [4:0] r;
+  
+  hypCal hypotenuse(.x_pos(normilizedX),
+							.y_pos(normilizedY),
+							.clk(clk),
+							.rst(~rst_n),
+							.r_sqroot(r)
+							);
+
+	assign R = video_active ? ((r[3:0] == adjustedAngle[3:0])? {~foreGround[0],~foreGround[0]} : {background[0],background[0]}) : 2'b00;
+	assign G = video_active ? ((r[3:0] == adjustedAngle[3:0])? {foreGround[1],foreGround[1]} : {background[1],background[1]}) : 2'b00;
+	assign B = video_active ? ((r[3:0] == adjustedAngle[3:0])? {foreGround[2],foreGround[2]} : {background[2],background[2]}) : 2'b00;
+
+
+
+
+endmodule
